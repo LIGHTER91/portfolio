83494c1aa0b718343b38115830d1684761c93785
LIGHTER91 <lucien.lachaud@etu.u-bordeaux.fr>
2024-10-22 13:24:38 +0200

    Resolved Router link out

--- src/components/Sticky/DropDownMenu.tsx
+++ src/components/Sticky/DropDownMenu.tsx
@@ -231,10 +231,7 @@ const DropdownMenu: React.FC<DropdownMenuProps> = ({ className = '', projects })
               {currentProject.sector && <p className='p-p'><strong>Secteur:</strong> {currentProject.sector}</p>}
               {currentProject.description && <p className='description'>{currentProject.description}</p>}
               {currentProject.readMoreLink && (
-  <Link to={currentProject.readMoreLink} onClick={(e) => {
-    e.preventDefault();
-    window.location.href = currentProject.readMoreLink?? '#';;
-  }}>
+  <Link to={currentProject.readMoreLink}>
     Read more
   </Link>
 )}
